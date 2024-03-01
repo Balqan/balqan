65aaa67fc23b39dddccc35c8a862da2de858c769
Balqan <161827756+Balqan@users.noreply.github.com>
2024-03-01 11:19:01 +0100

    first

---
+++ assets/javascript/main.js
@@ -0,0 +1,50 @@
+const scrollers = document.querySelectorAll(".scroller");
+if (!window.matchMedia("(prefers-reduced-motion: reduce").matches) {
+  addAnimation();
+}
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    scroller.setAttribute("data-animated", true);
+
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerConternt = Array.from(scrollerInner.children);
+
+    scrollerConternt.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
+
+let buttons = document.querySelectorAll(".info .row .content button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    let span = this.querySelector("span");
+    let paragraph = this.nextElementSibling;
+
+    paragraph.classList.toggle("show");
+
+    if (paragraph.classList.contains("show")) {
+      span.textContent = "";
+    } else {
+      span.textContent = "More";
+    }
+  });
+});
+<<<<<<< HEAD
+
+const heartIcon = document.querySelector(".heart.fa-regular");
+
+heartIcon.addEventListener("mouseenter", () => {
+  heartIcon.classList.remove("fa-regular");
+  heartIcon.classList.add("fa-solid");
+});
+
+heartIcon.addEventListener("mouseleave", () => {
+  heartIcon.classList.remove("fa-solid");
+  heartIcon.classList.add("fa-regular");
+});
+=======
+>>>>>>> 11b2f25b6a99ded10db583feb6485917b602225f
